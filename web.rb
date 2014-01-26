da17fe3206b13300699e603bad680e284bc121eb
WeladwBanat <99@hotmail.cz>
2014-01-27 01:22:37 +0200

    Rename index.erb to web.rb

--- web.rb
+++ web.rb
@@ -1,4 +1,3 @@
-
    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
