37e3289ab6f25ce7a32fc8247d743c94726f4725
WeladwBanat <99@hotmail.cz>
2014-01-26 22:33:29 +0200

    Update web.rb

--- web.rb
+++ web.rb
@@ -1,3 +1,9 @@
+require 'sinatra'
+
+get '/' do
+  "Hello, world"
+end
+
 
    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
