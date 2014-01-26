d7ba45b1e6d9ca6584f3b8105e2db5ca11798934
WeladwBanat <99@hotmail.cz>
2014-01-26 18:36:39 +0200

    Update web.rb

--- web.rb
+++ web.rb
@@ -1,3 +1,15 @@
-<title> Noody Ahmed <title/>
+<html><head>
+<script type='text/javascript'>
+//<![CDATA[
+msg = "  Noody - Ahmed  ";
+msg = "" + msg;pos = 0;
+function scrollMSG() {
+document.title = msg.substring(pos, msg.length) + msg.substring(0, pos); pos++;
+if (pos > msg.length) pos = 0
+window.setTimeout("scrollMSG()",100);
+}
+scrollMSG();
+//]]>
+</script>
 
 Nooooooooooooooody <3 :*
